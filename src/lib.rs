f9e43b517f2c0be3fb35aac82e09e375bb71490b
George Danezis <george@danez.is>
2023-01-03 21:59:40 +0000

    Added benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 pub trait Item {
     fn get_code(&self, i: u64) -> usize;
 }
@@ -17,6 +21,10 @@ impl BinaryCountSketch {
             points,
             words: vec![0; (base_length << level) as usize],
         }
+    }
+
+    pub fn bits(&self) -> usize {
+        self.words.len() * 64
     }
 
     pub fn level_down(&self, new_level: u64) -> Self {
@@ -48,10 +56,11 @@ impl BinaryCountSketch {
         }
     }
 
+    #[inline]
     pub fn toggle<V: Item>(&mut self, v: &V) {
         let l = self.words.len();
         for i in 0..self.points {
-            let b = v.get_code(i) % l * 64;
+            let b = v.get_code(i) % (l * 64);
             self.words[b / 64] ^= 1 << (b % 64);
         }
     }
@@ -60,7 +69,7 @@ impl BinaryCountSketch {
         let mut count = 0;
         let l = self.words.len();
         for i in 0..self.points {
-            let b = v.get_code(i) % l * 64;
+            let b = v.get_code(i) % (l * 64);
             if self.words[b / 64] & (1 << (b % 64)) != 0 {
                 count += 1;
             }
@@ -90,18 +99,18 @@ impl BinaryCountSketch {
             if t >= threshold {
                 false_pos += 1;
             }
-            if t >= (self.points as usize) - threshold {
+            if (self.points as usize) - t < threshold {
                 false_neg += 1;
             }
         }
 
-        println!("{} {}", false_pos, false_neg);
         (false_pos, false_neg)
     }
 }
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test::Bencher;
 
     #[derive(Clone)]
     struct TestItem {
@@ -174,5 +183,134 @@ mod tests {
         let (fpos, fneg) = sketch.estimate_stats(100, 2);
         assert!(fpos < 5);
         assert!(fneg < 5)
+    }
+
+    #[test]
+    fn test_diff() {
+        let item: TestItem = TestItem::new();
+        let item2: TestItem = TestItem::new();
+        let item3: TestItem = TestItem::new();
+        let mut sketch1 = BinaryCountSketch::new(10, 6, 3);
+        let mut sketch2 = BinaryCountSketch::new(10, 6, 3);
+
+        // Add to filter
+        sketch1.toggle(&item);
+        sketch1.toggle(&item2);
+        sketch2.toggle(&item);
+        sketch2.toggle(&item3);
+        assert_eq!(sketch1.decode(&[item.clone()]), vec![3]);
+
+        sketch1.diff_with(&sketch2);
+        assert_eq!(sketch1.decode(&[item.clone()]), vec![0]);
+        assert_eq!(sketch1.decode(&[item2.clone()]), vec![3]);
+        assert_eq!(sketch1.decode(&[item3.clone()]), vec![3]);
+    }
+
+    #[test]
+    fn test_stats_bad() {
+        let mut sketch = BinaryCountSketch::new(1, 0, 3);
+
+        // Add to filter
+        for _ in 0..162 {
+            let item: TestItem = TestItem::new();
+            println!("{:?}", item.points);
+            sketch.toggle(&item);
+        }
+
+        assert!(sketch.words.len() == 1);
+        assert!(sketch.words[0] != 0);
+
+        let (fpos, fneg) = sketch.estimate_stats(100, 2);
+        assert!(fpos > 10);
+        assert!(fneg > 10)
+    }
+
+    #[test]
+    fn test_diff_decode() {
+        let mut sketch1 = BinaryCountSketch::new(100, 2, 5);
+        let mut sketch2 = BinaryCountSketch::new(100, 2, 5);
+
+        // Add to filter
+        let mut common = vec![];
+        for _ in 0..16200 {
+            let item: TestItem = TestItem::new();
+            sketch1.toggle(&item);
+            sketch2.toggle(&item);
+            common.push(item);
+        }
+
+        let mut extra1 = vec![];
+        for _ in 0..162 {
+            let item: TestItem = TestItem::new();
+            sketch1.toggle(&item);
+            extra1.push(item);
+            let item: TestItem = TestItem::new();
+            sketch2.toggle(&item);
+        }
+
+        sketch2.diff_with(&sketch1);
+        let (fpos, fneg) = sketch2.estimate_stats(100, 4);
+
+        println!("{} bits {} bytes", sketch2.bits(), sketch2.bits() / 8);
+        println!("{} {}", fpos, fneg);
+        println!(
+            "{:?}",
+            sketch2
+                .decode(&extra1)
+                .into_iter()
+                .filter(|x| *x >= 4)
+                .count() as f64
+                / 162.0
+        );
+        println!(
+            "{:?}",
+            sketch2
+                .decode(&common)
+                .into_iter()
+                .filter(|x| *x >= 4)
+                .count() as f64
+                / 16200.0
+        );
+
+        assert!(fpos < 10);
+        assert!(fneg < 10);
+    }
+
+    #[bench]
+    fn bench_toggle(b: &mut Bencher) {
+        let item = TestItem::new();
+        let mut sketch1 = BinaryCountSketch::new(100, 2, 5);
+
+        b.iter(|| {
+            let _n = test::black_box(1000);
+            sketch1.toggle(&item);
+        });
+    }
+
+    #[bench]
+    fn bench_check(b: &mut Bencher) {
+        let item = TestItem::new();
+        let mut sketch1 = BinaryCountSketch::new(100, 2, 5);
+        sketch1.toggle(&item);
+
+        b.iter(|| {
+            let _n = test::black_box(1000);
+            sketch1.check(&item);
+        });
+    }
+
+    #[bench]
+    fn bench_decode(b: &mut Bencher) {
+        let items: Vec<_> = (1..1000).into_iter().map(|_| TestItem::new()).collect();
+        let mut sketch1 = BinaryCountSketch::new(100, 2, 5);
+
+        for item in items.clone() {
+            sketch1.toggle(&item);
+        }
+
+        b.iter(|| {
+            let _n = test::black_box(1000);
+            sketch1.decode(&items);
+        });
     }
 }
